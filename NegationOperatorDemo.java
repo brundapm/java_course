72592ce21a9ed87274e0754cd02f089ad85eb88e
Brundapm <bbbpapu@gmail.com>
2021-09-23 00:18:42 +0530

    write NegationOperatorDemo program

---
+++ NegationOperatorDemo.java
@@ -0,0 +1,13 @@
+class NegationOperatorDemo{
+
+public static void main(String[]args){
+
+  boolean b = true;
+  boolean result = !b;
+
+System.out.println("b: "+b);//false
+System.out.println("result: "+result);//false
+
+}
+
+}
