6b78ba7b85528f8c9c1ba80e1c93caecab70f78d
Brundapm <bbbpapu@gmail.com>
2021-09-23 00:13:31 +0530

    write IncrementOperatorDemo1 program

---
+++ IncrementOperatorDemo1.java
@@ -0,0 +1,21 @@
+class IncrementOperatorDemo1{
+
+public static void main(String[]args){
+
+System.out.println("pre increment operatation.....");
+  int b = 10;
+  int s = ++b;
+
+System.out.println("b: "+b);
+System.out.println("s: "+s);
+
+System.out.println("post increment operatation.....");
+  int m = 70;
+  int n = m++;
+
+System.out.println("m: "+m);
+System.out.println("n: "+n);
+
+}
+
+}
