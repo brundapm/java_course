09a0349af359f15538f0469460317b25226105a8
Brundapm <bbbpapu@gmail.com>
2021-09-23 00:26:33 +0530

    write DataTypesTest program

---
+++ DataTypesTest.java
@@ -0,0 +1,32 @@
+class DataTypesTest{
+public static void main(String[] args){
+//integer
+  byte b = 10;
+  System.out.println("b :"+b);
+  
+  short s = 30000;
+  System.out.println("s :"+s);
+
+  int i = 100000;
+  System.out.println("i :"+i);
+ 
+  long l = 10000000000l;
+  System.out.println("i :"+i);
+
+//floating
+  float f = 10.00f;
+  System.out.println("f :"+f);
+
+  double d = 70.50f;
+  System.out.println("d :"+d);
+
+//char
+  char c = '7';
+  System.out.println("c :"+c);
+
+//boolean
+  boolean boo = true;//faulse
+  System.out.println("boo :"+boo);
+}
+
+}
