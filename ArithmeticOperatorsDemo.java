2570ed81289697fbda298e5a2956c0030d1e8e6b
Brundapm <bbbpapu@gmail.com>
2021-09-22 23:19:11 +0530

    write ArithmeticOperatorsDemo

---
+++ ArithmeticOperatorsDemo.java
@@ -0,0 +1,31 @@
+class ArithmeticOperatorsDemo{
+
+public static void main(String[] args){
+
+int a = 10;
+int b = 20;
+int addResult = a+b;
+System.out.println("addResult : "+addResult);
+
+int x = 10;
+int y = 20;
+int subResult = x-y;
+System.out.println("subResult : "+subResult);
+
+int c = 10;
+int d = 20;
+int mulResult = c*d;
+System.out.println("mulResult : "+mulResult);
+
+int e = 10;
+int f = 20;
+int modResult = e%f;
+System.out.println("modResult : "+modResult);
+
+int g = 10;
+int h = 20;
+int divResult = g/h;
+System.out.println("divResult : "+divResult);
+
+}
+}
