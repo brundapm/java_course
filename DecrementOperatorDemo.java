54834fff4d5b3d52285da30f2f0f86d8d207a879
Brundapm <bbbpapu@gmail.com>
2021-09-23 00:15:32 +0530

    write DecrementOperatorDemo program

---
+++ DecrementOperatorDemo.java
@@ -0,0 +1,21 @@
+class DecrementOperatorDemo{
+
+public static void main(String[]args){
+
+System.out.println("pre increment operatation.....");
+  int i = 1;
+  int j = --i;
+
+System.out.println("i: "+i);
+System.out.println("j: "+j);
+
+System.out.println("post increment operatation.....");
+  int m = 50;
+  int n = m--;
+
+System.out.println("m: "+m);
+System.out.println("n: "+n);
+
+}
+
+}
