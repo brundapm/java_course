53edb483e63c033fa7e734532658ca317778337f
Brundapm <bbbpapu@gmail.com>
2021-09-27 07:51:11 +0530

    write RelationalOperators program

---
+++ RelationalOperators.java
@@ -0,0 +1,41 @@
+class RelationalOperators{
+
+  public static void main(String[] args){
+
+   int i = 10;
+   int j = 20;
+   int k = 20;
+  
+   boolean result1 = i < j;
+  System.out.println("i < j :" +result1);
+
+   boolean result2 = j < i;
+  System.out.println("j < i :" +result2);
+
+   boolean result3 = i > j;
+  System.out.println("i > j :" +result3);
+
+   boolean result4 = j > i;
+  System.out.println("j > i :" +result4);
+
+   boolean result5 = i <= k;
+  System.out.println("i <= k :" +result5);
+
+   boolean result6 = i >= j;
+  System.out.println("i >= j :" +result6);
+
+   boolean result7 = i == k;
+  System.out.println("i == k :" +result7);
+
+   boolean result8 = i != k;
+  System.out.println("i != k :" +result8);
+
+   boolean result9 = j != k;
+  System.out.println("j != k :" +result9);
+
+   boolean result10 = k == j;
+  System.out.println("k == j :" +result10);
+
+}
+
+  }
