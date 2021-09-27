5ddb92f7d2f92b2246c875703288aff30f86937b
Brundapm <bbbpapu@gmail.com>
2021-09-27 07:44:10 +0530

    write AssignmentOperator program

---
+++ AssignmentOperator.java
@@ -0,0 +1,18 @@
+class AssignmentOperator{
+
+  public static void main(String[] args){
+
+   int a = 5;
+   int b = 7;
+   b+=a;
+  System.out.println("b : " +b);
+
+   int i = 10;
+   int j = 20;
+   j/=i;
+  System.out.println("j : " +j); 
+
+
+}
+
+  }
