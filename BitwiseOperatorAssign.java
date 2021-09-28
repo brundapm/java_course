0fe6efbbb141c451d31a7b7082a7896793ae0f8f
Brundapm <bbbpapu@gmail.com>
2021-09-28 11:20:37 +0530

    write BitwiseOperatorAssign program

---
+++ BitwiseOperatorAssign.java
@@ -0,0 +1,18 @@
+class BitwiseOperatorAssign{
+
+ public static void main(String[] args){
+
+   int a = 8 ;
+   int b = 5 ;
+System.out.println( "Bitwise AND operator");
+System.out.println( a>b & a++<b);
+
+System.out.println( "Bitwise OR operator");
+System.out.println( a>b | a++<b);
+
+System.out.println( "Bitwise XOR operator");
+System.out.println(a>b ^ a++<b );
+
+ }
+
+}
