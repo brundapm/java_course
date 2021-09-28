e86c02dff113f94064d18a4d29296b8bc3fd1f6a
Brundapm <bbbpapu@gmail.com>
2021-09-28 11:25:09 +0530

    write ShiftOperator program

---
+++ ShiftOperator.java
@@ -0,0 +1,12 @@
+class ShiftOperator{
+
+ public static void main(String[] args){
+
+   int a=11;
+
+System.out.println("left shift operator:a<<2="+(a<<2));
+System.out.println("right shift operator:a>>2="+(a>>2));
+
+ }
+
+}
