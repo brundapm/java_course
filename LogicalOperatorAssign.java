5416eaa53d9425dd0a032ac0ea29a2c800d3a019
Brundapm <bbbpapu@gmail.com>
2021-09-28 11:18:53 +0530

    write LogicalOperatorAssign program

---
+++ LogicalOperatorAssign.java
@@ -0,0 +1,18 @@
+class LogicalOperatorAssign{
+
+ public static void main(String[] args){
+
+  int a = 100 ;
+  int b = 200 ;
+System.out.println( "Logical AND operator");
+System.out.println( a>b && a++<b);
+
+System.out.println( "Logical OR operator");
+System.out.println( a>b || a++<b);
+
+System.out.println( "Logical NOT operator");
+System.out.println( !(a>b));
+
+ }
+
+}
