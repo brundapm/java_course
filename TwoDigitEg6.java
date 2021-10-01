9521cbb76059a88c3170bac363177ee2a8d1b37a
Brundapm <bbbpapu@gmail.com>
2021-10-01 17:55:24 +0530

    write TwoDigitEg6 program

---
+++ TwoDigitEg6.java
@@ -0,0 +1,14 @@
+class TwoDigitEg6
+{
+public static void main(String args[]){
+    int num=121235;
+
+    
+       if(num >= 10 && num < 100){
+            System.out.println("it is two digit number");
+        }else
+        {
+           System.out.println("it is Not Two-Digit Number");
+            }
+        }
+    }
