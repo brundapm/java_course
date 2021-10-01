bb629d70ccc28247d72e89ced98c83548f5df6b2
Brundapm <bbbpapu@gmail.com>
2021-10-01 18:00:32 +0530

    write HelloDevelopersJokersF program

---
+++ HelloDevelopersJokersF.java
@@ -0,0 +1,12 @@
+public class HelloDevelopersJokersF{
+
+    public static void main(String[] args) {
+
+       boolean helloDevelopers = false;    
+        
+        if(helloDevelopers == true){
+            System.out.println("helloDevelopers");}
+        else{
+            System.out.println("helloJokers");}
+    }
+}
